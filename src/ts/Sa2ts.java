1c8ea0bec7d2a41d6cafb18937429b4129479327
Djibril Mimouni <67635844+Hellil@users.noreply.github.com>
2026-03-20 15:29:42 +0100

    TP3 logic done, but some parts missing

--- src/ts/Sa2ts.java
+++ src/ts/Sa2ts.java
@@ -1,4 +1,5 @@
 package ts;
+import sa.ErrorException;
 import sa.SaAppel;
 import sa.SaDecFonc;
 import sa.SaDecTab;
@@ -7,6 +8,7 @@ import sa.SaDepthFirstVisitor;
 import sa.SaNode;
 import sa.SaVarIndicee;
 import sa.SaVarSimple;
+import util.Error;
 
 public class Sa2ts extends SaDepthFirstVisitor <Void> {
     enum Context {
@@ -21,11 +23,24 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     
     public Ts getTableGlobale(){return this.tableGlobale;}
 
-    public Sa2ts()
+    public Sa2ts(SaNode root) // param dans CM mais pas dans TP
     {
-	tableGlobale = new Ts();
-	tableLocaleCourante = null;
-	context = Context.GLOBAL;
+        tableGlobale = new Ts();
+        tableLocaleCourante = null;
+        context = Context.GLOBAL;
+
+        // suite dans le CM mais pas dans TP
+        try{
+            root.accept(this);
+            if(tableGlobale.getFct("main") == null)
+                throw new ErrorException(Error.TS, "la fonction main n'existe pas");
+        }
+        catch(ErrorException e){
+            System.err.print("ERREUR TABLE DES SYMBOLES : ");
+            System.err.println(e.getMessage());
+            System.exit(e.getCode());
+        }
+        catch(Exception e){}
     }
 
     public void defaultIn(SaNode node)
@@ -41,24 +56,77 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     public Void visit(SaDecVar node) throws Exception
     {
         defaultIn(node);
+        Ts table = (tableLocaleCourante != null)? tableLocaleCourante : tableGlobale;
+        TsItemVar item = null;
+        if(context == Context.GLOBAL){ // si globale et decl var, vérifie si existe déja, sinon crée (rechDecl)
+            if (tableGlobale.getVar(node.getNom()) == null){
+                item = tableGlobale.addVar(node.getNom(), node.getType());
+            }
+        }
+        else if(context == Context.LOCAL){ // si locale et decl var, vérifie si existe déja, sinon crée (rechDecl)
+            if (tableLocaleCourante.getVar(node.getNom()) == null){
+                item = tableLocaleCourante.addVar(node.getNom(), node.getType());
+            }
+        }
+        // TsItemVar item = rechercheDeclarative(node.getNom()); 
+        if(item == null || item.portee != table){
+            if(context == Context.PARAM)
+                node.setTsItem(table.addParam(node.getNom(), node.getType()));
+            else
+                node.setTsItem(table.addVar(node.getNom(), node.getType()));
+        }
+        else{
+            throw new ErrorException(Error.TS, "il existe déjà une variable " + node.getNom());
+        }
+        /* old code
         if(context == Context.GLOBAL){
             tableGlobale.addVar(node.getNom(), node.getType());
         }
         else{
             tableLocaleCourante.addVar(node.getNom(), node.getType());
         }
+        */
         defaultOut(node);
         return null;
     }
 
+    /*
+    
+
+    */
+
     public Void visit(SaDecTab node) throws Exception{
         defaultIn(node);
+        Ts table = (tableLocaleCourante != null)? tableLocaleCourante : tableGlobale;
+        TsItemVar item = null;
+        if(context == Context.GLOBAL){ // si globale et decl var, vérifie si existe déja, sinon crée (rechDecl)
+            if (tableGlobale.getVar(node.getNom()) == null){
+                item = tableGlobale.addTab(node.getNom(), node.getType(), node.getTaille());
+            }
+        }
+        else if(context == Context.LOCAL){ // si locale et decl var, vérifie si existe déja, sinon crée (rechDecl)
+            if (tableLocaleCourante.getVar(node.getNom()) == null){
+                item = tableLocaleCourante.addTab(node.getNom(), node.getType(), node.getTaille());
+            }
+        }
+        // TsItemVar item = rechercheDeclarative(node.getNom()); 
+        if(item == null || item.portee != table){ // TODO: code ok pour var, peut etre pas pour tab
+            if(context == Context.PARAM)
+                node.setTsItem(table.addParam(node.getNom(), node.getType()));
+            else
+                node.setTsItem(table.addTab(node.getNom(), node.getType(), node.getTaille()));
+        } // TODO: fin du todo
+        else{
+            throw new ErrorException(Error.TS, "il existe déjà une variable " + node.getNom());
+        }
+        /* old code
         if(context == Context.GLOBAL){
             tableGlobale.addTab(node.getNom(), node.getType(), node.getTaille());
         }
         else{
             tableLocaleCourante.addTab(node.getNom(), node.getType(), node.getTaille());
         }
+        */
         defaultOut(node);
         return null;
     }
@@ -67,7 +135,8 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     {
         defaultIn(node);
         Ts tableLocale = new Ts();
-        tableGlobale.addFct(node.getNom(), node.getTypeRetour(), node.getParametres().length(), tableLocale, node);
+        TsItemFct item = tableGlobale.addFct(node.getNom(), node.getTypeRetour(), node.getParametres().length(), tableLocale, node);
+        //setTsItem ?
         tableLocaleCourante = tableLocale;
         context = Context.PARAM;
         if(node.getParametres() != null) node.getParametres().accept(this);
@@ -96,7 +165,7 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
         if(var == null){
             throw new Exception("Variable non déclarée : " + node.getNom());
         }
-
+        //setTsItem ?
         defaultOut(node);
         return null;
     }
@@ -117,8 +186,9 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
         if(tab == null){
             throw new Exception("Tableau non déclaré : " + node.getNom());
         }
-
+        //setTsItem ici ? +
         node.getIndice().accept(this);
+        //setTsItem ou ici ? -
         defaultOut(node);
         return null;
     }
@@ -132,7 +202,15 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
             throw new Exception("Fonction non déclarée : " + node.getNom());
         }
 
+        //setTsItem mais pas de setTsItem dans SaAppel
+
+        int argLen = 0;
         if(node.getArguments() != null) node.getArguments().accept(this);
+        argLen = node.getArguments().length();
+        if(argLen != fct.getNbArgs()){
+            throw new ErrorException(Error.TS, "nombre d'arguments incorrect pour la fonction " + node.getNom());
+        }
+        //vérifier si nombre args correct ?
         defaultOut(node);
         return null;
     }
