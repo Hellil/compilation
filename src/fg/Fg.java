0a0a2fa334da46be130d55c8d826082ec4d10370
Djibril Mimouni <67635844+Hellil@users.noreply.github.com>
2026-04-06 21:32:16 +0200

    TP7 4th try

--- src/fg/Fg.java
+++ src/fg/Fg.java
@@ -1,4 +1,5 @@
 package fg;
+
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
@@ -30,6 +31,7 @@ import nasm.NasmLabel;
 import nasm.NasmMov;
 import nasm.NasmMul;
 import nasm.NasmNot;
+import nasm.NasmOperand;
 import nasm.NasmOr;
 import nasm.NasmPop;
 import nasm.NasmPush;
@@ -47,34 +49,71 @@ import util.graph.Graph;
 import util.graph.Node;
 import util.graph.NodeList;
 
-public class Fg implements NasmVisitor <Void> {
+public class Fg implements NasmVisitor<Void> {
     public Nasm nasm;
     public Graph graph;
-    Map< NasmInst, Node> inst2Node;
-    Map< Node, NasmInst> node2Inst;
-    Map< String, NasmInst> label2Inst;
+    Map<NasmInst, Node> inst2Node;
+    Map<Node, NasmInst> node2Inst;
+    Map<String, NasmInst> label2Inst;
 
     public Fg(Nasm nasm){
         this.nasm = nasm;
-        this.inst2Node = new HashMap<NasmInst, Node>();
-        this.node2Inst = new HashMap<Node, NasmInst>();
-        this.label2Inst = new HashMap<String, NasmInst>();
+        this.inst2Node = new HashMap<>();
+        this.node2Inst = new HashMap<>();
+        this.label2Inst = new HashMap<>();
         this.graph = new Graph();
 
-        // créer noeuds et gestion labels
+        // Création des noeuds + mapping labels
         for (NasmInst inst : nasm.sectionText) {
             Node node = this.graph.newNode();
-            this.inst2Node.put(inst, node);
-            this.node2Inst.put(node, inst);
-            
+            inst2Node.put(inst, node);
+            node2Inst.put(node, inst);
+
             if (inst.label != null) {
-                this.label2Inst.put(inst.label.toString(), inst);
+                // OK ici car NasmLabel.toString() = val
+                label2Inst.put(inst.label.toString(), inst);
             }
         }
 
-        // créer arcs avec visiteur
+        // Création des arcs
         for (NasmInst inst : nasm.sectionText) {
             inst.accept(this);
+        }
+    }
+
+    // --- utilitaires ---
+
+    private void addSequentialEdge(NasmInst inst) {
+        int index = nasm.sectionText.indexOf(inst);
+        if (index < nasm.sectionText.size() - 1) {
+            NasmInst nextInst = nasm.sectionText.get(index + 1);
+            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
+        }
+    }
+
+    private void addJumpEdge(NasmInst inst, NasmOperand address) {
+        if (address == null) return;
+
+        String labelName = null;
+
+        // cas 1 : label direct (call f, jmp f, etc.)
+        if (address instanceof NasmLabel) {
+            labelName = ((NasmLabel) address).val;
+        }
+        // cas 2 : adresse contenant un label
+        else if (address instanceof NasmAddress) {
+            NasmAddress addr = (NasmAddress) address;
+
+            if (addr.val instanceof NasmLabel) {
+                labelName = ((NasmLabel) addr.val).val;
+            }
+        }
+
+        if (labelName != null) {
+            NasmInst targetInst = label2Inst.get(labelName);
+            if (targetInst != null) {
+                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
+            }
         }
     }
 
@@ -91,7 +130,7 @@ public class Fg implements NasmVisitor <Void> {
                 System.err.println("Error: " + e.getMessage());
             }
         }
-        
+
         for(NasmInst nasmInst : nasm.sectionText){
             Node n = this.inst2Node.get(nasmInst);
             out.print(n + " : ( ");
@@ -103,275 +142,84 @@ public class Fg implements NasmVisitor <Void> {
         }
     }
 
-    // --- Instructions purement séquentielles ---
+    // --- Instructions séquentielles ---
+    public Void visit(NasmAdd inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmSub inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmMul inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmDiv inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmMov inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmPush inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmPop inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmOr inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmAnd inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmXor inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmNot inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmCmp inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmInst inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmInt inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmEmpty inst) { addSequentialEdge(inst); return null; }
+    public Void visit(NasmCall inst) { addSequentialEdge(inst); return null; }
 
-    public Void visit(NasmAdd inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmSub inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmMul inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmDiv inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmMov inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmPush inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmPop inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmOr inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmAnd inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmXor inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmNot inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmCmp inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmInst inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmInt inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmEmpty inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    public Void visit(NasmCall inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        return null;
-    }
-
-    // --- Sauts Conditionnels (Séquentiel + Branchement) ---
-
+    // --- Sauts conditionnels ---
     public Void visit(NasmJe inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
     public Void visit(NasmJne inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
     public Void visit(NasmJl inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
     public Void visit(NasmJle inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
     public Void visit(NasmJg inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
     public Void visit(NasmJge inst) {
-        int index = nasm.sectionText.indexOf(inst);
-        if (index < nasm.sectionText.size() - 1) {
-            NasmInst nextInst = nasm.sectionText.get(index + 1);
-            graph.addEdge(inst2Node.get(inst), inst2Node.get(nextInst));
-        }
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addSequentialEdge(inst);
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
-    // --- Saut Inconditionnel (Branchement uniquement) ---
-
+    // --- Saut inconditionnel ---
     public Void visit(NasmJmp inst) {
-        if (inst.address != null) {
-            NasmInst targetInst = label2Inst.get(inst.address.toString());
-            if (targetInst != null) {
-                graph.addEdge(inst2Node.get(inst), inst2Node.get(targetInst));
-            }
-        }
+        addJumpEdge(inst, inst.address);
         return null;
     }
 
-    // --- Fin du flux (Aucun arc) ---
-
+    // --- Fin ---
     public Void visit(NasmRet inst) {
         return null;
     }
 
-    // --- Visiteurs des opérandes et pseudo-instructions ---
-    
+    // --- Opérandes ---
     public Void visit(NasmAddress operand) { return null; }
     public Void visit(NasmConstant operand) { return null; }
     public Void visit(NasmLabel operand) { return null; }
     public Void visit(NasmRegister operand) { return null; }
-    
+
     public Void visit(NasmResb pseudoInst) { return null; }
     public Void visit(NasmResw pseudoInst) { return null; }
     public Void visit(NasmResd pseudoInst) { return null; }
     public Void visit(NasmResq pseudoInst) { return null; }
     public Void visit(NasmRest pseudoInst) { return null; }
-    
+
     public Void visit(NasmExpPlus exp) { return null; }
     public Void visit(NasmExpMinus exp) { return null; }
     public Void visit(NasmExpTimes exp) { return null; }
--- src/fg/FgSolution.java
+++ src/fg/FgSolution.java
@@ -17,117 +17,136 @@ import util.graph.NodeList;
 import util.intset.IntSet;
 
 public class FgSolution{
-	int iterNum = 0;
-	public Nasm nasm;
-	Fg fg;
-	public Map< NasmInst, IntSet> use;
-	public Map< NasmInst, IntSet> def;
-	public Map< NasmInst, IntSet> in;
-	public Map< NasmInst, IntSet> out;
+    int iterNum = 0;
+    public Nasm nasm;
+    Fg fg;
+    public Map<NasmInst, IntSet> use;
+    public Map<NasmInst, IntSet> def;
+    public Map<NasmInst, IntSet> in;
+    public Map<NasmInst, IntSet> out;
 
-	public FgSolution(Nasm nasm, Fg fg){
-		this.nasm = nasm;
-		this.fg = fg;
-		this.use = new HashMap< NasmInst, IntSet>();
-		this.def = new HashMap< NasmInst, IntSet>();
-		this.in =  new HashMap< NasmInst, IntSet>();
-		this.out = new HashMap< NasmInst, IntSet>();
+    public FgSolution(Nasm nasm, Fg fg){
+        this.nasm = nasm;
+        this.fg = fg;
+        this.use = new HashMap<>();
+        this.def = new HashMap<>();
+        this.in = new HashMap<>();
+        this.out = new HashMap<>();
 
-		int size = nasm.getTempCounter();
+        int size = nasm.getTempCounter();
 
-		for(NasmInst inst : nasm.sectionText){
-			use.put(inst, new IntSet(size));
-			def.put(inst, new IntSet(size));
-			in.put(inst, new IntSet(size));
-			out.put(inst, new IntSet(size));
+        // Initialisation use / def / in / out
+        for(NasmInst inst : nasm.sectionText){
+            use.put(inst, new IntSet(size));
+            def.put(inst, new IntSet(size));
+            in.put(inst, new IntSet(size));
+            out.put(inst, new IntSet(size));
 
-			extractUseDef(inst.source, inst.srcDef, inst.srcUse, use.get(inst), def.get(inst));
-			extractUseDef(inst.destination, inst.destDef, inst.destUse, use.get(inst), def.get(inst));
-		}
+            extractUseDef(inst.source, inst.srcUse, inst.srcDef, use.get(inst), def.get(inst));
+            extractUseDef(inst.destination, inst.destUse, inst.destDef, use.get(inst), def.get(inst));
+        }
 
-		boolean changed = true;
-		while(changed) {
-			changed = false;
-			iterNum++;
+        boolean changed = true;
 
-			for(NasmInst inst : nasm.sectionText) {
-				Node n = fg.inst2Node.get(inst);
+        while(changed) {
+            changed = false;
+            iterNum++;
 
-				IntSet oldIn = in.get(inst).copy();
-				IntSet oldOut = out.get(inst).copy();
+            // ✅ PARCOURS BACKWARD (IMPORTANT)
+            for(int i = nasm.sectionText.size() - 1; i >= 0; i--) {
+                NasmInst inst = nasm.sectionText.get(i);
 
-				IntSet newOut = new IntSet(size);
+                Node n = fg.inst2Node.get(inst);
 
-				for(NodeList nList = n.succ(); nList != null; nList = nList.tail) {
-					Node succNode = nList.head;
-					NasmInst succInst = fg.node2Inst.get(succNode);
-					newOut.union(in.get(succInst));
-				}
-				out.put(inst, newOut);
-				
+                IntSet oldIn = in.get(inst).copy();
+                IntSet oldOut = out.get(inst).copy();
 
-				IntSet newIn = use.get(inst).copy();
-				IntSet tempOut = out.get(inst).copy().minus(def.get(inst));
-				newIn.union(tempOut);
-				in.put(inst, newIn);
+                // --- calcul OUT ---
+                IntSet newOut = new IntSet(size);
 
-				if(!oldIn.equal(in.get(inst)) || !oldOut.equal(out.get(inst))) {
-					changed = true;
-				}
-			}
-		}
-	} 
+                for(NodeList nList = n.succ(); nList != null; nList = nList.tail) {
+                    Node succNode = nList.head;
+                    NasmInst succInst = fg.node2Inst.get(succNode);
 
-	private void extractUseDef(Object oper, boolean isUse, boolean isDef, IntSet useSet, IntSet defSet) {
-		if (oper == null) return;
+                    if (succInst != null) {
+                        newOut.union(in.get(succInst));
+                    }
+                }
 
-		if (oper instanceof NasmRegister) {
-			NasmRegister r1 = (NasmRegister) oper;
-			if (r1.isGeneralRegister()) {
-				if (isUse) useSet.add(r1.val);
-				if (isDef) defSet.add(r1.val);
-			}
-		}
-		else if (oper instanceof NasmAddress) {
-			NasmAddress address = (NasmAddress) oper;
-			// Dans une adresse (ex: [ebp + r1]), les composants sont TOUJOURS utilises (lus),
-			// meme si l'adresse globale est la destination d'une ecriture.
-			extractUseDef(address.val, true, false, useSet, defSet);
-		}
-		else if (oper instanceof NasmExpPlus) {
-			NasmExpPlus exp = (NasmExpPlus) oper;
-			extractUseDef(exp.op1, true, false, useSet, defSet);
-			extractUseDef(exp.op2, true, false, useSet, defSet);
-		}
-		else if (oper instanceof NasmExpMinus) {
-			NasmExpMinus exp = (NasmExpMinus) oper;
-			extractUseDef(exp.op1, true, false, useSet, defSet);
-			extractUseDef(exp.op2, true, false, useSet, defSet);
-		}
-		else if (oper instanceof NasmExpTimes) {
-			NasmExpTimes exp = (NasmExpTimes) oper;
-			extractUseDef(exp.op1, true, false, useSet, defSet);
-			extractUseDef(exp.op2, true, false, useSet, defSet);
-		}
-	}
+                out.put(inst, newOut);
 
-	public void affiche(String baseFileName){
-		String fileName;
-		PrintStream out = System.out;
+                // --- calcul IN ---
+                IntSet newIn = use.get(inst).copy();
 
-		if (baseFileName != null){
-			try {
-				fileName = baseFileName + ".fgs";
-				out = new PrintStream(fileName);
-			}
-			catch (IOException e) {
-				System.err.println("Error: " + e.getMessage());
-			}
-		}
+                IntSet temp = out.get(inst).copy();
+                temp.minus(def.get(inst));
 
-		out.println("iter num = " + iterNum);
-		for(NasmInst nasmInst : this.nasm.sectionText){
-			out.println("use = "+ this.use.get(nasmInst) + " def = "+ this.def.get(nasmInst) + "\tin = " + this.in.get(nasmInst) + "\t \tout = " + this.out.get(nasmInst) + "\t \t" + nasmInst);
-		}
-	}
+                newIn.union(temp);
+
+                in.put(inst, newIn);
+
+                // --- test convergence ---
+                if(!oldIn.equal(in.get(inst)) || !oldOut.equal(out.get(inst))) {
+                    changed = true;
+                }
+            }
+        }
+    }
+
+    private void extractUseDef(Object oper, boolean isUse, boolean isDef, IntSet useSet, IntSet defSet) {
+        if (oper == null) return;
+
+        if (oper instanceof NasmRegister) {
+            NasmRegister r = (NasmRegister) oper;
+            if (r.isGeneralRegister()) {
+                if (isUse) useSet.add(r.val);
+                if (isDef) defSet.add(r.val);
+            }
+        }
+        else if (oper instanceof NasmAddress) {
+            NasmAddress addr = (NasmAddress) oper;
+            extractUseDef(addr.val, true, false, useSet, defSet);
+        }
+        else if (oper instanceof NasmExpPlus) {
+            NasmExpPlus exp = (NasmExpPlus) oper;
+            extractUseDef(exp.op1, true, false, useSet, defSet);
+            extractUseDef(exp.op2, true, false, useSet, defSet);
+        }
+        else if (oper instanceof NasmExpMinus) {
+            NasmExpMinus exp = (NasmExpMinus) oper;
+            extractUseDef(exp.op1, true, false, useSet, defSet);
+            extractUseDef(exp.op2, true, false, useSet, defSet);
+        }
+        else if (oper instanceof NasmExpTimes) {
+            NasmExpTimes exp = (NasmExpTimes) oper;
+            extractUseDef(exp.op1, true, false, useSet, defSet);
+            extractUseDef(exp.op2, true, false, useSet, defSet);
+        }
+    }
+
+    public void affiche(String baseFileName){
+        String fileName;
+        PrintStream ps = System.out;
+
+        if (baseFileName != null){
+            try {
+                fileName = baseFileName + ".fgs";
+                ps = new PrintStream(fileName);
+            }
+            catch (IOException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+
+        ps.println("iter num = " + iterNum);
+        for(NasmInst inst : this.nasm.sectionText){
+            ps.println(
+                "use = " + use.get(inst) +
+                " def = " + def.get(inst) +
+                "\tin = " + in.get(inst) +
+                "\tout = " + out.get(inst) +
+                "\t\t" + inst
+            );
+        }
+    }
 }
